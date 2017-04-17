c093e62ff6611443e7728eda668d24f5f555d93b
Romain Lesur <romain.lesur@justice.gouv.fr>
2017-04-17 22:00:59 +0200

    Modify Test parameters

--- tests/testthat/test-DBItest.R
+++ tests/testthat/test-DBItest.R
@@ -1,3 +1,13 @@
-DBItest::make_context(SASEG(), NULL)
+DBItest::make_context(
+  drv = SASEG("C:\\Program Files\\SAS94M2\\SASEnterpriseGuide\\7.1\\SASEGScripting.dll"), 
+  connect_args = list(
+    DLLFilePath = "C:\\Program Files\\SAS94M2\\SASEnterpriseGuide\\7.1\\SASEGScripting.dll",
+    profile = rstudioapi::askForPassword("Profile Name:"),
+    server = rstudioapi::askForPassword("Server Name:")
+  ),
+  set_as_default = TRUE,
+  tweaks = tweaks(omit_blob_tests = TRUE),
+  name = "Romain Lesur's profile"
+)
 DBItest::test_getting_started()
 DBItest::test_driver()
